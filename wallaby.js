a714794df58dfe5881f402c9783e6aa5fd73e1cd
Artem Govorov <artem.govorov@gmail.com>
2016-04-17 11:13:20 +1000

    Update wallaby.js
    Pointed browserify to wallaby cache

--- wallaby.js
+++ wallaby.js
@@ -1,13 +1,16 @@
+var path = require('path');
 var wallabify = require('wallabify');
-var wallabyPostprocessor = wallabify({
-      paths: ['src']
-  }
-  // you may also pass an initializer function to chain other
-  // browserify options, such as transformers
-  // , b => b.exclude('mkdirp').transform(require('babelify'))
-);
 
-module.exports = function () {
+module.exports = function (wallaby) {
+
+  var wallabyPostprocessor = wallabify({
+      paths: [path.join(wallaby.projectCacheDir, 'src')]
+    }
+    // you may also pass an initializer function to chain other
+    // browserify options, such as transformers
+    // , b => b.exclude('mkdirp').transform(require('babelify'))
+  );
+
   return {
     // set `load: false` to all of the browserified source files and tests,
     // as they should not be loaded in browser, 
